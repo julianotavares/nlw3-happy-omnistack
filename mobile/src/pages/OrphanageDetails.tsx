c3b168549f7fb8c1dc1b32a3b9da53b0d1cb9b2b
Juliano Tavares <juliano.tavaress@gmail.com>
2020-12-24 16:00:47 -0300

    Added Routes

---
+++ mobile/src/pages/OrphanageDetails.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import React from 'react';
+import { View } from 'react-native';
+
+export default function OrphanageDetails() {
+  return <View />;
+}
