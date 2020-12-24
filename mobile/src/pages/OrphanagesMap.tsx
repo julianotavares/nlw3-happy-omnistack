bc2ddf24b149a576a796ec00832ff828e88f049a
Juliano Tavares <juliano.tavaress@gmail.com>
2020-12-24 15:01:09 -0300

    Added footer

---
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const pages: React.FC = () => {
+  return <View />;
+};
+
+export default pages;
